c2359985f58ab22e6d09f1c68f27a8ef329de0a7
Fareed.Faisal <fareed.faisal@paconsulting.com>
2025-01-31 15:27:14 +0000

    DPPB-909: Add deletion_protection flag for UI resource and update ingress to all traffic

--- terraform-int/container_runtime.tf
+++ terraform-int/container_runtime.tf
@@ -18,7 +18,8 @@ locals {
 resource "google_cloud_run_v2_service" "service_portal_service" {
   name     = local.Service_ui_cloud_run_service_name
   location = var.region
-  ingress  = "INGRESS_TRAFFIC_INTERNAL_LOAD_BALANCER"
+  deletion_protection = false
+  ingress  = "INGRESS_TRAFFIC_ALL"
 
   template {
     service_account = var.execution_service_account
