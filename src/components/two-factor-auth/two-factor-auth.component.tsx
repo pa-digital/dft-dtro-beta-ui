2b2bfdcebb8299b0e64c6e44a8fb3353c44a4951
Ben Pauley <ben.pauley@paconsulting.com>
2025-02-08 19:04:00 +0000

    Update props name

--- src/components/two-factor-auth/two-factor-auth.component.tsx
+++ src/components/two-factor-auth/two-factor-auth.component.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import styles from "./two-factor-auth.module.css";
 
-interface TwoFactorAuthProps {
+interface TwoFactorAuthComponentProps {
   n: number;
 }
 
-const TwoFactorAuthComponent: React.FC<TwoFactorAuthProps> = ({ n }) => {
+const TwoFactorAuthComponent: React.FC<TwoFactorAuthComponentProps> = ({
+  n,
+}) => {
   return (
     <div className={styles.container}>
       {Array.from({ length: n }, (_, index) => (
