1b3036a5239a977e3336c372f1a83bdb4cdc9287
Ben Pauley <ben.pauley@paconsulting.com>
2025-02-09 11:10:28 +0000

    Added disabled logic for button component

--- src/components/button/button.component.tsx
+++ src/components/button/button.component.tsx
@@ -5,21 +5,32 @@ import styles from "./button.module.css";
 export enum ButtonType {
   Primary = "primary",
   Secondary = "secondary",
-  Disabled = "disabled",
 }
 
 interface ButtonComponentProps {
   type: ButtonType;
   text: string;
+  disabled?: boolean;
+  onClick: () => void;
 }
 
-const ButtonComponent: React.FC<ButtonComponentProps> = ({ type, text }) => {
+const ButtonComponent: React.FC<ButtonComponentProps> = ({
+  type,
+  text,
+  disabled,
+  onClick,
+}) => {
   return (
     <button
       className={classNames(styles.button, {
         [styles.primary]: type === ButtonType.Primary,
         [styles.secondary]: type === ButtonType.Secondary,
+        [styles.disabled]: disabled
       })}
+      onClick={() => {
+        if (disabled) return;
+        onClick();
+      }}
     >
       {text}
     </button>
