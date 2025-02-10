98903c5d23d6612c874bc4f136a9546145a36c10
Ben Pauley <ben.pauley@paconsulting.com>
2025-02-10 12:17:22 +0000

    Added navbar component

---
+++ src/components/navbar/navbar.component.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import styles from "./navbar.module.css";
+
+const NavbarComponent: React.FC = () => {
+  return <div className={styles.navbar}></div>;
+};
+
+export default NavbarComponent;
