09aa1080bd0ad2ae9993fdd0250c4aa7c18547d1
Ben Pauley <ben.pauley@paconsulting.com>
2025-02-10 12:17:35 +0000

    Added footer component

---
+++ src/components/footer/footer.component.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import styles from "./footer.module.css";
+
+const FooterComponent: React.FC = () => {
+  return <div className={styles.footer}></div>;
+};
+
+export default FooterComponent;
