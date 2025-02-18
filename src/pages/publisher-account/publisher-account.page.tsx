3b5edba1140f153f7005294f05abbf916eeac58a
Cameron Auld <cameron.auld@paconsulting.com>
2025-02-18 16:23:38 +0000

    Added axios api caller to call a dummy api to determine if user can request a new publisher app

--- src/pages/publisher-account/publisher-account.page.tsx
+++ src/pages/publisher-account/publisher-account.page.tsx
@@ -1,12 +1,30 @@
-import React from "react";
+import {useState, useEffect } from "react";
 import styles from "./publisher-account.module.css";
 import TextComponent, { TypographyType } from "../../components/text/typography.component";
 import ActionLinkComponent from "../../components/action-link/action-link.component";
 import NavLinkComponent from "../../components/nav-link/nav-link.component";
+import axios from "axios";
 
-var canPublishApps = true;
+const apiUrl = "http://127.0.0.1:5000/canPublish";
 
 const PublisherAccountPage: React.FC = () => {
+  const [canPublishApps, setCanPublishApps] = useState<boolean | null>(null);
+  const [loading, setLoading] = useState<boolean>(true);
+  const [error, setError] = useState<string | null>(null);
+
+
+  useEffect(() => {
+    axios.get(apiUrl)
+      .then(response => setCanPublishApps(response.data.canPublish))
+      .catch(() => setError(`Error fetching data from ${apiUrl}`))
+      .finally(() => setLoading(false));
+  }, []);
+
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>{error}</p>;
+
+
   return (
     <div className={styles.content}>
         <div className={styles.headerContainer}>
@@ -17,7 +35,7 @@ const PublisherAccountPage: React.FC = () => {
             <TextComponent type={TypographyType.SubDescription} content="Generate app credentials for use with the D-TRO test environment."/>
             <ActionLinkComponent link="#" text="Create new consumer app" enabled={true}/>
             <TextComponent type={TypographyType.SubDescription} content="Generate app credentials to consume data from the D-TRO test environment."/>
-            <ActionLinkComponent link="#" text="Request new publisher app" enabled={canPublishApps}/>
+            <ActionLinkComponent link="#" text="Request new publisher app" enabled={canPublishApps ?? false}/>
             {canPublishApps ? 
                 <TextComponent type={TypographyType.SubDescription} content="Request credentials for a new app to publish to the D-TRO production environment."/>
               : <TextComponent type={TypographyType.SubDescription} content="To request access to publish to the DTRO production environment, please contact dtro-cso@dft.org.uk"/>
