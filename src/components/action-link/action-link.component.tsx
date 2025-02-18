6a089aeb77c1b7343548a85ee23604332fffaaab
Cameron Auld <cameron.auld@paconsulting.com>
2025-02-18 09:25:16 +0000

    cretaed a action link component and publisher account page

---
+++ src/components/action-link/action-link.component.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import styles from "./action-link.module.css";
+import ActionArrow from "../../assets/action-arrow.svg";
+
+interface ActionLinkComponentProps {
+  text: string;
+  link: string;
+  enabled?: boolean;
+}
+
+const ActionLinkComponent: React.FC<ActionLinkComponentProps> = ({ text, link, enabled = true}) => {
+    if (enabled == false) {
+        return (
+        <div className={`${styles.actionContent} ${styles.disabledLink}`}>
+            <a href={link}>{text}</a>
+            <img src={ActionArrow}></img>
+        </div>
+        )
+    }
+   return (
+    <div className={styles.actionContent}>
+        <a href={link}>{text}</a>
+        <img src={ActionArrow}></img>
+    </div>
+  );
+};
+
+export default ActionLinkComponent;
---
+++ src/pages/publisher-account/publisher-account.page.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+import styles from "./publisher-account.module.css";
+import TextComponent, { TypographyType } from "../../components/text/typography.component";
+import ActionLinkComponent from "../../components/action-link/action-link.component";
+import NavLinkComponent from "../../components/nav-link/nav-link.component";
+
+const PublisherAccountPage: React.FC = () => {
+  return (
+    <div className={styles.content}>
+        <div className={styles.headerContainer}>
+            <NavLinkComponent link="#" text="Sign In"/>
+            <TextComponent type={TypographyType.SubHeading} content="Publisher account"/>
+        </div>
+            <ActionLinkComponent link="#" text="Create new test app"/>
+            <TextComponent type={TypographyType.SubDescription} content="Generate app credentials for use with the D-TRO test environment."/>
+            <ActionLinkComponent link="#" text="Create new consumer app"/>
+            <TextComponent type={TypographyType.SubDescription} content="Generate app credentials to consume data from the D-TRO test environment."/>
+            <ActionLinkComponent link="#" text="Request new publisher app" enabled={false}/>
+            <TextComponent type={TypographyType.SubDescription} content="Request credentials for a new app to publish to the D-TRO production environment."/>
+            <ActionLinkComponent link="#"  text="View/ retrieve credentials"/>
+            <TextComponent type={TypographyType.SubDescription} content="View your existing app credentials for test and production environments and generate new credentials for an existing app."/>
+    </div>
+  );
+};
+
+export default PublisherAccountPage;
