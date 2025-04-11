672d9fc11a3ec4938577a0f4a6738fa9ca40fe3a
Fareed.Faisal <fareed.faisal@paconsulting.com>
2025-04-11 11:06:56 +0100

    update axios with env updates

--- src/utils/axios-instance.ts
+++ src/utils/axios-instance.ts
@@ -1,12 +1,8 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-    baseURL: import.meta.env.VITE_BASE_URL || 'https://localhost:5001',
-    withCredentials: true,
-    headers: {
-        "App-Id": import.meta.env.VITE_APP_ID,
-        "Email": import.meta.env.VITE_EMAIL
-    }
+    baseURL: import.meta.env.VITE_API_URL || 'https://localhost:5001',
+    withCredentials: true
 });
 
 export default axiosInstance;
